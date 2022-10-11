328c0334f615847104fa1a6673fca9e2b5d35ad7
Gregorius Ferdinand <ferdinandgregorius@yahoo.com>
2022-10-11 17:59:45 +0700

    Re-modified Login.java. Added a function for the Register Button to redirect users to the Register page.

--- app/src/main/java/com/example/mobilesoftwareengineeringquiz/Login.java
+++ app/src/main/java/com/example/mobilesoftwareengineeringquiz/Login.java
@@ -2,13 +2,38 @@ package com.example.mobilesoftwareengineeringquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    private Button loginBtn;
+    private Button registerBtn;
+    private EditText inputUsername;
+    private EditText inputPassword;
+    private String usernameString;
+    private String passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBtn = findViewById(R.id.login_btn);
+        registerBtn = findViewById(R.id.register_btn1);
+        inputUsername = findViewById(R.id.username_field);
+        inputPassword = findViewById(R.id.password_field);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Register.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
